6f48e4e0b3f5b3682b40052751295ed11c8704de
Jason Klaaste <jason@Jasons-MacBook-Pro.local>
2018-03-26 02:53:40 +0200

    The app now stores the last check result

--- src/index.js
+++ src/index.js
@@ -11,44 +11,53 @@ class App extends React.Component {
                 "id": 0,
                 "url": "https://test.cognitioncom/api/status",
                 "status": undefined,
+                "lastStatus": undefined,
             },
             {
                 "id": 1,
                 "url": "https://ord.dev.stackworx.io/health",
                 "status": undefined,
+                "lastStatus": undefined,
             },
             {
                 "id": 2,
                 "url": "https://api.durf.dev.stackworx.io/health",
                 "status": undefined,
+                "lastStatus": undefined,
             },
             {
                 "id": 3,
                 "url": "https://prima.run/health",
                 "status": undefined,
+                "lastStatus": undefined,
             },
             {
                 "id": 4,
                 "url": "https://stackworx.io/",
                 "status": undefined,
+                "lastStatus": undefined,
             },
             // Test servers
             {
                 "id": 5,
                 "url": "https://httpstat.us/404",
                 "status": undefined,
+                "lastStatus": undefined,
             },
             {
                 "id": 6,
                 "url": "https://httpstat.us/300",
                 "status": undefined,
+                "lastStatus": undefined,
             },
             {
                 "id": 7,
                 "url": "https://httpstat.us/203",
                 "status": undefined,
+                "lastStatus": undefined,
             },
-        ]
+        ],
+        checkNumber: 0
     }
 
     fetchStatus(){
@@ -57,14 +66,25 @@ class App extends React.Component {
         Object.keys(copyState.endPoints).map(key => (
             request(copyState.endPoints[key].url)
             .on('response', response => {
+                copyState.endPoints[key].lastStatus =
+                    this.state.checkNumber ?
+                    copyState.endPoints[key].status :
+                    "";
                 copyState.endPoints[key].status = response.statusCode;
                 this.setState(copyState);
             })
             .on('error', error => {
-                copyState.endPoints[key].status = 'Error';
+                copyState.endPoints[key].lastStatus =
+                    this.state.checkNumber ?
+                    copyState.endPoints[key].status :
+                    "";
+                copyState.endPoints[key].status = 'Error retieving status';
                 this.setState(copyState);
             })
         ))
+
+        copyState.checkNumber++;
+        this.setState(copyState)
     }
 
     componentDidMount(){
@@ -80,7 +100,10 @@ class App extends React.Component {
             <div>
                 <h1>Build Monitor</h1>
                 <div>
-                    <Servers key={this.state.endPoints.id} serverList={this.state.endPoints}/>
+                    <Servers 
+                        key={this.state.endPoints.id} 
+                        serverList={this.state.endPoints}
+                    />
                 </div>
             </div>
         );
