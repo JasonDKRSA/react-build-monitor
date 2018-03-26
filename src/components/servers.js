9b520c0ba4dc5580a6015ac1e75e1d176a30d99f
Jason Klaaste <jason@Jasons-MacBook-Pro.local>
2018-03-26 14:34:25 +0200

    Added function to split servers by development or production

--- src/components/servers.js
+++ src/components/servers.js
@@ -1,20 +1,65 @@
 import React from "react";
 import Server from "./server";
 
+function checkUrlForDev(url){
+    const devStrings = ["dev","qa","test"];
+
+    const devIndex = devStrings.map(devString => {
+        return url.indexOf(devString);
+    });
+
+    const singleIndex = devIndex.reduce((prevValue, currValue) => {
+        return prevValue > currValue ? prevValue : currValue;
+    });
+
+    return singleIndex;
+}
+
 const Servers = props => (
+    
     <span>
-        <h1><u>List of servers:</u></h1>
-        
-        <ul className="items">
-            {
-                Object.keys(props.serverList).map(key => (
-                    <Server 
-                        key={props.serverList[key].id} 
-                        server={props.serverList[key]} 
-                    />
-                ))
-            }
-        </ul>
+        <h2><u>List of servers:</u></h2>
+
+        <h2>Development Servers</h2>
+
+        <hr/>
+
+        {
+            <ul className="items">
+                {       
+                    Object.keys(props.serverList).map(key => (
+
+                        checkUrlForDev(props.serverList[key].url) > 0 &&
+
+                            <Server 
+                                key={props.serverList[key].id}
+                                server={props.serverList[key]}
+                            />
+                    ))
+                }
+            </ul>
+        }
+
+        <h2>Production Servers</h2>
+
+        <hr/>
+
+        {
+            <ul className="items">
+                {       
+                    Object.keys(props.serverList).map(key => (
+
+                        checkUrlForDev(props.serverList[key].url) < 0 &&
+
+                            <Server 
+                                key={props.serverList[key].id}
+                                server={props.serverList[key]}
+                            />
+                    ))
+                }
+            </ul>
+        }
+
     </span>
 )
 
